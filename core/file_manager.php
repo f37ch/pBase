c9931747b15f5c5b4bfe26c00f62dd66462981e5
f37ch <owltaver@gmail.com>
2023-10-07 19:08:33 +0300

    filter_filename improvements

--- core/file_manager.php
+++ core/file_manager.php
@@ -41,8 +41,7 @@ function calc_percent($val,$percent)
 	return $val*($percent/100); 
 }
 function filter_filename($filename){
-    $filename=mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])","",$filename);
-    $filename=mb_ereg_replace("([\.]{2,})","",$filename);
+    $filename=preg_replace("/[^a-zA-Z0-9.]/","",$filename);
     $filename=ltrim($filename,".-");
     $ext=pathinfo($filename,PATHINFO_EXTENSION);
     $filename=mb_strcut(pathinfo($filename,PATHINFO_FILENAME),0,50-($ext?strlen($ext)+1:0),mb_detect_encoding($filename)).($ext?".".$ext:"");//filename only 50 bite max.
