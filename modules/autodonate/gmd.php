ff2ba27e90ddd28afd245059b81ea7e0d5c836d0
f37ch <owltaver@gmail.com>
2023-11-18 04:38:47 +0200

    curl-less method (fixes sign err)

--- modules/autodonate/gmd.php
+++ modules/autodonate/gmd.php
@@ -2,27 +2,24 @@
 $methods["gmd"]=array("img"=>"https://i.imgur.com/KRvcJt1.png");
 if (isset($_GET["check"])){
 	include_once("../../core/db.php");
-	$amount=number_format($_GET["amount"],2,".","");// "1.50"
+	$amount=round($_GET["amount"]);
 	$steamid=$_GET["steamid"];
-    $svid=$_GET["svid"]==1?1424:1785;//your project ids from gmd panel
-	$projectSecret=$_GET["svid"]==1?"key1":"key2";//your project keys from gmd panel
+    $svid=$_GET["svid"]==1?1424:1785;//your project ids
+	$projectSecret=$_GET["svid"]==1?"key1":"key2";//your project secret keys
 	$params=[
         "sid"=>$steamid,
         "sum"=>$amount
     ];
-    $headers=[
-        "sign:".hash("sha256",$steamid."{up}".$amount."{up}".$projectSecret),
-        "project:".$svid
+    $options=[
+        "http"=>[
+            "header"=>"sign:".hash("sha256",$steamid."{up}".$amount."{up}".$projectSecret)."\r\n"."project:".$svid."\r\n"."User-Agent : Valve/Steam HTTP Client 1.0 (4000)\r\n",
+            "method"=>"POST",
+            "content"=>http_build_query($params),
+        ],
     ];
-    $ch=curl_init("https://gm-donate.net/api/url/getPayment");
-    curl_setopt($ch,CURLOPT_IPRESOLVE,CURL_IPRESOLVE_V4);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch,CURLOPT_POST,1);
-    curl_setopt($ch,CURLOPT_POSTFIELDS,$params);
-    curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
-    $data=curl_exec($ch);
-    curl_close($ch);
-    $ara=json_decode($data,true);
+    $context=stream_context_create($options);
+    $result=file_get_contents("https://gm-donate.net/api/url/getPayment",false,$context);
+    $ara=json_decode($result,true);
 	header("Location: ".$ara["data"]);
 }elseif(isset($_GET["accept"])){
 	include_once("../../core/rcon.php");
