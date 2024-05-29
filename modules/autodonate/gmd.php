ee7c693e5a7f5ccbc45e5b48853dc23f5e4d7d6f
f37ch <owltaver@gmail.com>
2024-05-30 01:54:01 +0300

    somehow its fixes 403 issue

--- modules/autodonate/gmd.php
+++ modules/autodonate/gmd.php
@@ -9,19 +9,26 @@ if (isset($_GET["check"])){
     $svid=$server["sv_name"]=="Cinema"?1424:1785;//your project ids from gmd panel
 	$projectSecret=$svid==1424?"cinemakey":"rpkey";//your project secret keys
 	$params=[
-        "sid"=>$steamid,
-        "sum"=>$amount
-    ];
-    $options=[
-        "http"=>[
-            "header"=>"sign:".hash("sha256",$steamid."{up}".$amount."{up}".$projectSecret)."\r\n"."project:".$svid."\r\n"."User-Agent : Valve/Steam HTTP Client 1.0 (4000)\r\n",
-            "method"=>"POST",
-            "content"=>http_build_query($params),
-        ],
-    ];
-    $context=stream_context_create($options);
-    $result=file_get_contents("https://gm-donate.net/api/url/getPayment",false,$context);
-    $ara=json_decode($result,true);
+	    "sid"=>$steamid,
+	    "sum"=>$amount
+	];
+	$options=[
+	    "http"=>[
+	        "header" => [
+	            "sign: ".hash("sha256",$steamid."{up}".$amount."{up}".$projectSecret),
+	            "project: ".$svid,
+	            "User-Agent: Valve/Steam HTTP Client 1.0 (4000)"
+	        ],
+	        "method"=>"POST",
+	        "content"=>http_build_query($params),
+	    ],
+	];
+	$context=stream_context_create($options);
+	$result=file_get_contents("https://gm-donate.net/api/url/getPayment",false,$context);
+	if ($result===FALSE) {
+	    die('Error occurred');
+	}
+	$ara=json_decode($result,true);
 	header("Location: ".$ara["data"]);
 }elseif(isset($_GET["accept"])){
 	include_once("../../core/rcon.php");
